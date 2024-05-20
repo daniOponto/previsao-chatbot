55a1e7a5e06306eb1983263aba5be6b8df69521e
daniOponto <168240785+daniOponto@users.noreply.github.com>
2024-05-20 20:33:19 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,119 +1,35 @@
 import pandas as pd
 import streamlit as st
-from datetime import datetime
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
-# Função para a aplicação de previsão de entrega
-def previsao_entrega():
-    # Carregar os dados
-    df = pd.read_excel('bancoDeDadosPI.xlsx')
+# Carregar os dados
+df = pd.read_excel('bancoDeDadosPI.xlsx')
 
-    # Convertendo as colunas de data para o tipo datetime
-    df['order_date'] = pd.to_datetime(df['order_date'], dayfirst=True)
-    df['ship_date'] = pd.to_datetime(df['ship_date'], dayfirst=True)
+# Convertendo as colunas de data para o tipo datetime
+df['order_date'] = pd.to_datetime(df['order_date'], dayfirst=True)
+df['ship_date'] = pd.to_datetime(df['ship_date'], dayfirst=True)
 
-    # Diferença em dias entre as datas de pedido e entrega
-    df['days_difference'] = (df['ship_date'] - df['order_date']).dt.days
+# Diferença em dias entre as datas de pedido e entrega
+df['days_difference'] = (df['ship_date'] - df['order_date']).dt.days
 
-    # Dados em conjunto de treinamento e teste
-    X = df[['ship_mode', 'country', 'city', 'state', 'category']]
-    y = df['days_difference']
+# Dados em conjunto de treinamento e teste
+X = df[['ship_mode', 'country', 'city', 'state', 'category']]
+y = df['days_difference']
 
-    # Colunas que serão codificadas
-    categorical_columns = ['ship_mode', 'country', 'city', 'state', 'category']
+# Colunas que serão codificadas
+categorical_columns = ['ship_mode', 'country', 'city', 'state', 'category']
 
-    # Codificar as colunas categóricas e treinar o modelo
-    pipeline = Pipeline([
-        ('encoder', ColumnTransformer([('encoder', OneHotEncoder(), categorical_columns)], remainder='passthrough')),
-        ('model', KNeighborsRegressor(n_neighbors=5))
-    ])
+# Codificar as colunas categóricas e treinar o modelo
+pipeline = Pipeline([
+    ('encoder', ColumnTransformer([('encoder', OneHotEncoder(), categorical_columns)], remainder='passthrough')),
+    ('model', KNeighborsRegressor(n_neighbors=5))
+])
 
-    # Treinando o modelo
-    pipeline.fit(X, y)
-
-    # Configurar cores do site
-    st.markdown(
-        """
-        <style>
-        .css-1aumxhk {
-            color: white;
-        }
-        .css-1aumxhk a {
-            color: #1a73e8;
-        }
-        .css-1rhbuit {
-            color: #1a73e8;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    # Criar a interface gráfica
-    st.title('Previsão de Data de Entrega')
-
-    # Entrada do usuário
-    st.markdown("## Informações de Entrada")
-    col1, col2 = st.columns(2)
-    with col1:
-        country = st.selectbox("País:", df['country'].unique())
-        state = st.selectbox("Estado:", df['state'].unique())
-        city = st.selectbox("Cidade:", df['city'].unique())
-    with col2:
-        ship_mode = st.selectbox("Modo de Envio:", df['ship_mode'].unique())
-        category = st.selectbox("Categoria:", df['category'].unique())
-
-    # Botão para fazer a previsão
-    if st.button('Prever', key='prediction'):
-        # DataFrame com os dados de entrada
-        entrada = pd.DataFrame([[ship_mode, country, city, state, category]],
-                               columns=['ship_mode', 'country', 'city', 'state', 'category'])
-
-        # Previsão
-        y_pred = pipeline.predict(entrada)
-
-        # Exibir a previsão
-        st.markdown("## Resultado da Previsão")
-        st.success(f'A previsão para a data de entrega é de {round(y_pred[0], 2)} dias.')
-
-# Função para a aplicação do chatbot
-def chatbot():
-    st.title("Bem-vindo ao Atendimento Virtual da Hold Logistica!")
-    st.write("Olá, meu nome é Ingor! Como posso ajudá-lo hoje?")
-    while True:
-        opcao = st.selectbox("Escolha uma opção:", ('Informações sobre a empresa', 'Informações sobre o pedido', 'Sair do atendimento'))
-
-        if opcao == 'Informações sobre a empresa':
-            opcao_empresa = st.selectbox("O que deseja saber sobre nós?", ('Mais informações sobre a empresa', 'Serviços prestados', 'Como entrar em contato conosco'))
-            st.write(empresa_respostas.get(opcao_empresa, 'Opção inválida.'))
-        elif opcao == 'Informações sobre o pedido':
-            codigo_cliente = st.text_input("Insira o código do seu pedido:")
-            if codigo_cliente:
-                st.write(buscar_pedido(codigo_cliente))
-        elif opcao == 'Sair do atendimento':
-            st.write("Até logo!")
-            break
-
-# Função para a outra aplicação
-def outra_aplicacao():
-    st.write("Essa é a outra aplicação.")
-    # Coloque aqui o código da outra aplicação
-
-# Página principal
-def main():
-    st.sidebar.title('Menu')
-    app_mode = st.sidebar.selectbox("Selecione a aplicação:", ["Previsão de Entrega", "Outra Aplicação"])
-
-    if app_mode == "Previsão de Entrega":
-        previsao_entrega()
-    elif app_mode == "Outra Aplicação":
-        outra_aplicacao()
-
-# Carregar dados da planilha Excel para a aplicação do chatbot
-df = pd.read_excel('bdpi.xlsx')  # Ajuste o caminho do arquivo conforme necessário
+# Treinando o modelo
+pipeline.fit(X, y)
 
 # Respostas para as opções sobre a empresa
 empresa_respostas = {
@@ -134,5 +50,50 @@ def buscar_pedido(codigo_cliente):
         formatted_order += f"A categoria de entrega é {order['Ship Mode']} e seu produto {order['Category']} chegará em {order['Ship Date']}."
         return formatted_order
 
-if __name__ == "__main__":
-    main()
+# Função Chatbot
+def chatbot():
+    st.title("Bem-vindo ao Atendimento Virtual da Hold Logistica!")
+    st.write("Olá, meu nome é Ingor! Como posso ajudá-lo hoje?")
+    while True:
+        opcao = st.selectbox("Escolha uma opção:", ('Informações sobre a empresa', 'Informações sobre o pedido', 'Sair do atendimento'))
+
+        if opcao == 'Informações sobre a empresa':
+            opcao_empresa = st.selectbox("O que deseja saber sobre nós?", ('Mais informações sobre a empresa', 'Serviços prestados', 'Como entrar em contato conosco'))
+            st.write(empresa_respostas.get(opcao_empresa, 'Opção inválida.'))
+        elif opcao == 'Informações sobre o pedido':
+            codigo_cliente = st.text_input("Insira o código do seu pedido:")
+            if codigo_cliente:
+                st.write(buscar_pedido(codigo_cliente))
+        elif opcao == 'Sair do atendimento':
+            st.write("Até logo!")
+            break
+
+# Criar a interface gráfica
+st.title('Previsão de Data de Entrega')
+
+# Entrada do usuário
+st.markdown("## Informações de Entrada")
+col1, col2 = st.columns(2)
+with col1:
+    country = st.selectbox("País:", df['country'].unique())
+    state = st.selectbox("Estado:", df['state'].unique())
+    city = st.selectbox("Cidade:", df['city'].unique())
+with col2:
+    ship_mode = st.selectbox("Modo de Envio:", df['ship_mode'].unique())
+    category = st.selectbox("Categoria:", df['category'].unique())
+
+# Botão para fazer a previsão
+if st.button('Prever', key='prediction'):
+    # DataFrame com os dados de entrada
+    entrada = pd.DataFrame([[ship_mode, country, city, state, category]],
+                           columns=['ship_mode', 'country', 'city', 'state', 'category'])
+
+    # Previsão
+    y_pred = pipeline.predict(entrada)
+
+    # Exibir a previsão
+    st.markdown("## Resultado da Previsão")
+    st.success(f'A previsão para a data de entrega é de {round(y_pred[0], 2)} dias.')
+
+# Inicializando o chatbot
+chatbot()
