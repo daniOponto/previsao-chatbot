2f6506b55b9ab1f69462d5abae355ea03083b311
daniOponto <168240785+daniOponto@users.noreply.github.com>
2024-05-20 21:13:07 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -84,12 +84,12 @@ st.title('Previsão de Data de Entrega')
 st.markdown("## Informações de Entrada")
 col1, col2 = st.columns(2)
 with col1:
-    country = st.selectbox("País:", [''] + list(df['country'].unique()))
-    state = st.selectbox("Estado:", [''] + list(df['state'].unique()))
-    city = st.selectbox("Cidade:", [''] + list(df['city'].unique()))
+    country = st.selectbox("País:", df['country'].unique(), index=0)
+    state = st.selectbox("Estado:", df['state'].unique(), index=0)
+    city = st.selectbox("Cidade:", df['city'].unique(), index=0)
 with col2:
-    ship_mode = st.selectbox("Modo de Envio:", [''] + list(df['ship_mode'].unique()))
-    category = st.selectbox("Categoria:", [''] + list(df['category'].unique()))
+    ship_mode = st.selectbox("Modo de Envio:", df['ship_mode'].unique(), index=0)
+    category = st.selectbox("Categoria:", df['category'].unique(), index=0)
 
 # Botão para fazer a previsão
 if st.button('Prever', key='prediction'):
